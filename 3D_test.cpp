5d144ed32db613556c2b97260c6b19af9b586989
dshak1 <dshak1@cs.sfu.ca>
2025-10-11 23:57:18 -0700

    1

--- 3D_test.cpp
+++ 3D_test.cpp
@@ -3,7 +3,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include <iostream>
+#include <iostream> 
 
 GLFWwindow* StartGLU();
 void CreateVBOVAO(GLuint& VAO, GLuint& VBO, const float* vertices, size_t vertexCount);
